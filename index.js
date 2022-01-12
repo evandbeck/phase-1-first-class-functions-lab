84066f8fdf2379bca58578ba055155b53a7bfe5d
Evan Beck <evandrydenbeck@gmail.com>
2022-01-11 22:19:10 -0500

    commit tree 03db03244abf5aa23ef7c955049e0fee2bee3208

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = [];
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (fareMultiplier) {
+    return function (value) {
+        return fareMultiplier * value;
+    }
+}
+
+const fareDoubler = function (fareMultiplier) {
+    return fareMultiplier * 2;
+}
+
+const fareTripler = function (fareMultiplier) {
+    return fareMultiplier * 3;
+}
+
+const selectDifferentDrivers = function (drivers, func) {
+    return func(drivers);
+}
